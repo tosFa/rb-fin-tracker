cf77e100f0964e0bb175a1221d308c49d01c43c3
tosFa <hoti.fatos@gmail.com>
2016-12-01 18:41:07 +0100

    edit user profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,13 +10,18 @@ class User < ApplicationRecord
    		under_stock_limit? && !stock_already_added?(ticker_symbol)
    	end
 
-   	def under_stock_limit?
-   		(user_stocks.count < 10)
-   	end
+	def under_stock_limit?
+		(user_stocks.count < 10)
+	end
 
-   	def stock_already_added?(ticker_symbol)
-   		stock = Stock.find_by_ticker(ticker_symbol)
-   		return false unless stock
-   		user_stocks.where(stock_id: stock.id).exists?
-   	end
+	def stock_already_added?(ticker_symbol)
+		stock = Stock.find_by_ticker(ticker_symbol)
+		return false unless stock
+		user_stocks.where(stock_id: stock.id).exists?
+	end
+
+	def full_name
+		return "#{first_name} #{last_name}".strip if (first_name || last_name)
+		"Anonymous"
+	end
 end
